8105ce38b8ef97731623e596291184bd9776aaf5
jacklu <272918553@qq.com>
2022-02-19 15:47:14 +0800

    first commit

---
+++ src/constants/error-types.js
@@ -0,0 +1,15 @@
+const NAME_OR_PASSWORD_IS_REQUIRED = 'name_or_password_is_required'
+const USER_ALREAD_EXISTS = 'user_alread_exists'
+const USER_DOES_NOT_EXISTS = 'user_does_not_exists'
+const PASSWORD_IS_INCORRECT = 'password_is_incorrect'
+const UNAUTHORRAZATION = 'UNAUTHORIZATION'
+const UNPERMISSION = 'UNPERMISSION'
+
+module.exports = {
+    NAME_OR_PASSWORD_IS_REQUIRED,
+    USER_ALREAD_EXISTS,
+    USER_DOES_NOT_EXISTS,
+    PASSWORD_IS_INCORRECT,
+    UNAUTHORRAZATION,
+    UNPERMISSION
+}
